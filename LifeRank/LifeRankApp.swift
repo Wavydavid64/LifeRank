cc1d0d2a5401edc1f74c34ffabc5f0a91386aa97
Wavydavid64 <davidevanhoward64@gmail.com>
2026-09-04 19:37:51 -0700

    Initial Commit

---
+++ LifeRank/LifeRankApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeRankApp.swift
+//  LifeRank
+//
+//  Created by David Howard on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct LifeRankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
